c98ba6ad72b3ea0d4c79986ba8f351ac4d6b7541
CenrightTeaching <courtney.enright@icloud.com>
2026-07-01 00:42:44 +0100

    Add missing path_provider import to database_helper.dart

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,5 +1,6 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sales_tracker/models/sale.dart';
 import 'package:sales_tracker/models/item.dart';
 
